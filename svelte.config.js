7480e3e5b795e4ad0b88c5200060216924a9da89
Gees Brouwer <geesbrouwer@icloud.com>
2025-12-11 15:05:49 +0100

    d

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,26 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      fallback: '404.html'
-    }),
-    // Base path automatisch goed zetten
-    paths: {
-      base: '/'
-    },
-    // Let op: assets moeten in static/
-    appDir: '_app' // default, kan blijven
-  }
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/degeomaten-website'
+		},
+		prerender: {
+			handleHttpError: 'warn' // voorkomt crash bij 404
+		}
+	}
 };
 
 export default config;
